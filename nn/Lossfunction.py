b7aee78a3dc80c93a43e0ca21439b01f866654de
allenyoung414 <112870395+allenyoung414@users.noreply.github.com>
2022-11-21 23:36:39 +0800

    Add files via upload
    修改版

--- nn/Lossfunction.py
+++ nn/Lossfunction.py
@@ -2,37 +2,8 @@
 '''
 loss函数这块传给你的还是计算图节点和label值，计算图节点的话就通过.value属性访问到预测值，label就是numpy数组
 '''
-'''
-class Loss(object):
 
-    def MSELoss(self,x,y:Module):#x是真实值，y是预测值
-        assert len(x) == len(y.len())
-        x = np.array(x)
-        y = np.array(y)
-        loss = np.sum(np.square(x - y.value)) / len(x)
-        return loss
 
-    def L1Loss(self,x,y:list):
-        assert len(x) == len(y)
-        x = np.array(x)
-        y = np.array(y)
-        loss = np.sum(np.abs(x - y)) / len(x)
-        return loss
-
-    def CrossEntropy_loss(self,y_true,y_pred:list):
-        assert len(y_true) == len(y_pred)
-        loss = 0
-        for y, fx in zip(y_true, y_pred):
-            loss += -y * np.log(fx)
-        return loss
-
-    def softmax(self,x):
-        x_exp = np.exp(x)
-        x_sum = np.sum(x_exp, axis=1, keepdims=True)
-        s = x_exp / x_sum
-        return s
-
-'''
 
 
 
@@ -65,26 +36,55 @@ class L2loss:
         return np.sum(np.square(x-y))
     def backward(self):
         dx=2*self.x
-        return dx
+        self.x.backward(dx)
 
 
 
 class Entropy:
     def __init__(self):
-        self.nx = None
-        self.ny = None
-        self.dnx = None
+        self.x = None
+        self.y = None
+        self.dx = None
 
-    def loss(self, nx, ny):
-        self.nx = nx
-        self.ny = ny
-        loss = np.sum(- ny * np.log(nx))
+    def loss(self, x, y):
+        self.x = x
+        self.y = y
+        loss = np.sum(- y * np.log(x))
         return loss
 
     def backward(self):
-        self.dnx = - self.ny / self.nx
-        return self.dnx
+        self.dx = - self.y / self.x
+        self.x.backward(self.dx)
 
+
+"""
+np.random.seed(123)
+np.set_printoptions(precision=6, suppress=True, linewidth=80)
+
+x_numpy = np.random.random(27)
+y_numpy = np.random.random(27)
+x_torch = torch.tensor(x_numpy, requires_grad=True)
+y_torch = torch.tensor(y_numpy, requires_grad=True)
+
+loss_func_numpy = MSELoss()
+loss_func_torch = torch.nn.MSELoss().float()
+
+loss_numpy = loss_func_numpy(x_numpy, y_numpy)
+loss_torch = loss_func_torch(x_torch, y_torch)
+
+loss_torch.backward()
+dx_numpy, dy_numpy = loss_func_numpy.backward()
+
+print(loss_numpy)
+print(loss_torch.data.numpy())
+print("----------")
+print(dx_numpy)
+print(x_torch.grad.numpy())
+print("----------")
+print(dy_numpy)
+print(y_torch.grad.numpy())
+
+"""
 """
 0.116960116566
 0.1169601165663142
